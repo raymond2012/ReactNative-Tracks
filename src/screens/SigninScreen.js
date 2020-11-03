66d3917ad08f5b527b74cfa0c7e611399a90d0da
Raymond <raymond96521>
2020-11-03 18:40:45 +0800

    Screens navigations flow testing of the whole app

---
+++ src/screens/SigninScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const SigninScreen = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 48 }}>Signin Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SigninScreen;
---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+const SignupScreen = ({ navigation }) => {
+  return (
+    <>
+      <Text style={{ fontSize: 48 }}>Signup Screen</Text>
+      <Button
+        title="Go to Signin"
+        onPress={() => {
+          navigation.navigate("Signin");
+        }}
+      />
+      <Button
+        title="Go to main flow"
+        onPress={() => {
+          navigation.navigate("mainFlow");
+        }}
+      />
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SignupScreen;
